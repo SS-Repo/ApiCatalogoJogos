2d4b41e5c796e2f16496f11abed3314932e89223
Sergio <sergio.desenvolvedor.web@gmail.com>
2021-08-30 10:56:02 -0300

    first commit

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/Repositories/JogoSqlServerRepository.cs
@@ -0,0 +1,128 @@
+﻿using ApiCatalogoJogos.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public class JogoSqlServerRepository : IJogoRespository
+    {
+        private readonly SqlConnection sqlConnection;
+
+        public JogoSqlServerRepository(IConfiguration configurarion) 
+        {
+            sqlConnection = new SqlConnection(configurarion.GetConnectionString("Default"));
+        }
+        public async Task Atualizar(Jogo jogo)
+        {
+            var comando = $"update Jogos set Nome = '{jogo.Nome}', Prdutora = '{jogo.Produtora}', Preco = {jogo.Preco.ToString()}";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            await sqlConnection.CloseAsync();
+        }
+
+        public void Dispose()
+        {
+            sqlConnection?.Close();
+            sqlConnection?.Dispose();
+        }
+
+        public Task Inserir(Jogo jogo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Jogo>> Obter(int pagina, int quantidade)
+        {
+            var jogos = new List<Jogo>();
+
+            var comando = $"select * from Jogos order by id offset {((pagina - 1) * quantidade)} rows fetch next {quantidade} rows";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+            while (sqlDataReader.Read()) 
+            {
+                jogos.Add(new Jogo
+                {
+                    Id = (Guid)sqlDataReader["Id"],
+                    Nome = (string)sqlDataReader["Nome"],
+                    Produtora = (string)sqlDataReader["Produtora"],
+                    Preco = (double)sqlDataReader["Preco"]
+                });
+            }
+
+            await sqlConnection.CloseAsync();
+            return jogos;
+        }
+
+        public async Task<Jogo> Obter(Guid id)
+        {
+            Jogo jogo = null;
+
+            var comando = $"select * from Jogos where Id = '{id}'";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+            while (sqlDataReader.Read())
+            {
+                jogo = new Jogo
+                {
+                    Id = (Guid)sqlDataReader["Id"],
+                    Nome = (string)sqlDataReader["Nome"],
+                    Produtora = (string)sqlDataReader["Produtora"],
+                    Preco = (double)sqlDataReader["Preco"]
+                };
+            }
+
+            await sqlConnection.CloseAsync();
+
+            return jogo;
+        }
+
+        public async Task<List<Jogo>> Obter(string nome, string produtora)
+        {
+            var jogos = new List<Jogo>();
+
+            var comando = $"select * from Jogos where Nome = '{nome}' and Produtora = '{produtora}";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+            while (sqlDataReader.Read())
+            {
+                jogos.Add(new Jogo
+                {
+                    Id = (Guid)sqlDataReader["Id"],
+                    Nome = (string)sqlDataReader["Nome"],
+                    Produtora = (string)sqlDataReader["Produtora"],
+                    Preco = (double)sqlDataReader["Preco"]
+                });
+            }
+
+            await sqlConnection.CloseAsync();
+            
+            return jogos;
+        }
+
+        public async Task Remover(Guid id)
+        {
+            var comando = $"delete from Jogos where Id = '{id}'";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            await sqlConnection.CloseAsync();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+<h2>Digital Innovation: Avenade Bootcamp - Desenvolvendo um sistema de catalogo de jogos em .netcore</h2>
+
+ pré-requisitos do projeto:
+
+* .netcore v 5.0
+* .net cli 
+* Visual Code,Visual Studio ou sua IDE favorita.
+* Controle de versão GIT instalado na sua máquina.
+* Conta no GitHub para o armazenamento do seu projeto na nuvem.
+
+Abaixo, seguem links bem bacanas, sobre tópicos de desenvolvimento do app:
+
+* [Referência do Visual Code, para download](https://code.visualstudio.com/)
+* [.netcore](https://dotnet.microsoft.com/download)
+* [Site oficial do GIT](https://git-scm.com/)
+* [Site oficial do GitHub](http://github.com/)
+
+Email:sergio.desenvolvedor.web@gmail.com
